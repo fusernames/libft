60cab1d613db34201f988708d122c5f86962b0e6
Alexis CAROFF <alcaroff@e1r10p19.42.fr>
2018-08-13 07:13:43 +0200

    norme on lstcpy

--- ft_lstcpy.c
+++ ft_lstcpy.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alcaroff <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/13 07:13:20 by alcaroff          #+#    #+#             */
+/*   Updated: 2018/08/13 07:13:25 by alcaroff         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstcpy(t_list *lst)
@@ -10,7 +22,7 @@ t_list	*ft_lstcpy(t_list *lst)
 	{
 		new = ft_lstnewlast(NULL, 0, &cpy);
 		new->content = lst->content;
-		new->content_size = lst->content_size; 
+		new->content_size = lst->content_size;
 		lst = lst->next;
 	}
 	return (cpy);
